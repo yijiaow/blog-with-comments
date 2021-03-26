cff0fd716ce763e9c94382ec3673be38ea519125
Yijiao Wang <ttwyj250@gmail.com>
2021-03-25 23:47:10 -0700

    Event syncing by storing events in in-memory data structure

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,15 +4,31 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post('http://localhost:4000/events', event); // posts
-  axios.post('http://localhost:4001/events', event); // comments
-  axios.post('http://localhost:4002/events', event); // query
-  axios.post('http://localhost:4003/events', event); // moderation
+  events.push(event);
+
+  axios
+    .post('http://localhost:4000/events', event)
+    .catch((err) => console.log(err.message)); // posts
+  axios
+    .post('http://localhost:4001/events', event)
+    .catch((err) => console.log(err.message)); // comments
+  axios
+    .post('http://localhost:4002/events', event)
+    .catch((err) => console.log(err.message)); // query
+  axios
+    .post('http://localhost:4003/events', event)
+    .catch((err) => console.log(err.message)); // moderation
 
   res.sendStatus(200);
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
